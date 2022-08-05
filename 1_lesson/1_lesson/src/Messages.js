9f6d29851998038ef65a3d03f708168a614a5229
Nikolay <nick.chuchunov@yandex.ru>
2022-08-05 18:50:46 +0300

    завершение 1 практического задания

---
+++ 1_lesson/1_lesson/src/Messages.js
@@ -0,0 +1,17 @@
+
+function Messages(TextAlert)
+{
+    return (
+        <div >
+        <header >
+        <h3>Hello  {TextAlert}</h3>
+        </header>
+        </div>
+        );
+        
+
+};
+
+
+
+export default Messages 
